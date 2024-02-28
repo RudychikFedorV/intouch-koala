7be04e3784a9b6ae610b3cd2ffb556b3a3d7c6bd
RudychikFedorV <rudychik.fedor@gmail.com>
2024-02-28 21:38:12 +0200

    footer

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ function navToggle() {
 	document.getElementById("headerUl").classList.toggle("_active");
 	document.getElementById("html").classList.toggle("_overflow");
 }
+document.getElementById("copyright").textContent = new Date().getFullYear();
